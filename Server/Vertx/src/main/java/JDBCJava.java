1b40cb0f7682aef0ddda559de5dedab57fd75d13
Kate <kate.zhangyucheng@gmail.com>
2015-11-25 17:41:19 -0500

    add q3

--- Server/Vertx/src/main/java/JDBCJava.java
+++ Server/Vertx/src/main/java/JDBCJava.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class JDBCJava {
 	// JDBC driver name and database URL
@@ -41,11 +42,11 @@ public class JDBCJava {
 		} else if (q6 != -1) {
 			return doQ6(key);
 		}
-		
+
 		return "shouldn't goes here!";
 
 	}
-	
+
 	private String doQ5(String key) {
 		// TODO Auto-generated method stub
 		return null;
@@ -57,43 +58,94 @@ public class JDBCJava {
 	}
 
 	private String doQ3(String key) {
-		// TODO
-		return null;
+		StringBuilder sb = new StringBuilder();
+		Statement stmt = null;
+		key = key.substring(3);
+		String[] keys = key.split(",");
+		String id = keys[0];
+		String start = keys[1];
+		String end = keys[2];
+		int num = Integer.parseInt(keys[3]);
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			stmt = conn.createStatement();
+			String sql = "SELECT * from test3 where userid=\'" + id + "\' ";
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			String result = "";
+			while (rs.next()) {
+				result += rs.getString("value");
+			}
+			String[] posts = result.split("#&#");
+			int n = posts.length;
+			// add positive
+			for (int i=0; i<num; i++) {
+				String post = posts[i];
+				String[] ele = post.split(",");
+				String time = ele[0];
+				int score = Integer.parseInt(ele[1]);
+				if (score > 0 && time.compareTo(start)>=0 && time.compareTo(end) <= 0) {
+					sb.append(post);
+				} else if (score < 0) {
+					break;
+				}
+			}
+			
+			// add negative
+			for (int i=0; i<num; i++) {
+				String post = posts[n-i];
+				String[] ele = post.split(",");
+				String time = ele[0];
+				int score = Integer.parseInt(ele[1]);
+				if (score < 0 && time.compareTo(start)>=0 && time.compareTo(end) <= 0) {
+					sb.append(post);
+				} else if (score > 0) {
+					break;
+				}
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
 	}
-	
+
 	private String doQ4(String key) {
 		StringBuilder sb = new StringBuilder();
 		Statement stmt = null;
 		key = key.substring(3);
+		String[] keys = key.split(",");
+		String hashTag = keys[1];
+		String n = keys[0];
 		try {
-			Class.forName("com.mysql.jdbc.Driver");				 
+			Class.forName("com.mysql.jdbc.Driver");
 			stmt = conn.createStatement();
-			String[] keys = key.split(",");
-			String hashTag = keys[1];				
-			String n = keys[0];
-
-			String sql;
-			sql = "SELECT * from test5 where hashTag=\'" + hashTag + "\' ";
+			String sql = "SELECT * from test4 where hashTag=\'" + hashTag
+					+ "\' ";
 
 			ResultSet rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				String response = rs.getString("value");
 				response = response.replace("$shit$", "\t");
 				String[] posts = response.split("\t");
-				
-				
+
 				int size = Integer.parseInt(n);
 				int len = posts.length;
 
-				for(int i = 0; i < size && i < len; i++){
+				for (int i = 0; i < size && i < len; i++) {
 					String temp = posts[i];
 
 					int left = 0;
-					while(left != -1){
+					while (left != -1) {
 						left = temp.indexOf("$fuck$");
-						if(left != -1){
-							temp = temp.substring(0,left) + "\n" + temp.substring(left+6);
+						if (left != -1) {
+							temp = temp.substring(0, left) + "\n"
+									+ temp.substring(left + 6);
 						}
 					}
 					sb.append(temp);
@@ -113,15 +165,15 @@ public class JDBCJava {
 		StringBuilder sb = new StringBuilder();
 		Statement stmt = null;
 		key = key.substring(3);
+		int mid = key.indexOf(",");
+		String userid = key.substring(0, mid);
+		String timestamp = key.substring(mid + 1);
 		try {
 			// build sql
 			Class.forName("com.mysql.jdbc.Driver");
 			stmt = conn.createStatement();
-			int mid = key.indexOf(",");
-			String userid = key.substring(0, mid);
-			String timestamp = key.substring(mid + 1);
-			String sql;
-			sql = "SELECT post from test2 where userid=" + userid
+
+			String sql = "SELECT post from test2 where userid=" + userid
 					+ " and newDate=\'" + timestamp + "\';";
 			// query
 			ResultSet rs = stmt.executeQuery(sql);
