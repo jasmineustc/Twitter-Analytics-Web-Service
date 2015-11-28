fe095d8cf422845de65acff1775a03a0e46b1969
Kate <kate.zhangyucheng@gmail.com>
2015-11-27 23:19:12 -0500

    add install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install openjdk-8-jdk
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install mysql-server
+
+wget https://s3.amazonaws.com/teamprojectmysqlremote/mysql-connector-java-5.1.37-bin.jar
+wget https://s3.amazonaws.com/teamprojectmysqlremote/vert.x-3.1.0-full.tar.gz
+tar -vxzf vert.x-3.1.0-full.tar.gz
