5daac3ad01a95805b82b07c354d9e29697dce99a
Kate <kate.zhangyucheng@gmail.com>
2015-11-28 17:02:33 -0500

    change scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,3 +11,14 @@ sudo apt-get install mysql-server
 wget https://s3.amazonaws.com/teamprojectmysqlremote/mysql-connector-java-5.1.37-bin.jar
 wget https://s3.amazonaws.com/teamprojectmysqlremote/vert.x-3.1.0-full.tar.gz
 tar -vxzf vert.x-3.1.0-full.tar.gz
+
+wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+sudo apt-get install unzip
+unzip awscli-bundle.zip
+sudo apt-get install awscli
+
+mkdir q2data
+mkdir q3data
+mkdir q4data
+mkdir q5data
+mkdir q6data
